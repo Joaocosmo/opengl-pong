7179979a7739f049e1756133543fd0358419b290
mirelecostaovm <mirele@overmediacast.com>
2023-04-29 20:09:17 -0300

    Ajuste de formatação e padrão de nomes

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
+#include <math.h>
 
 using namespace std;
 
@@ -43,6 +44,21 @@ void DrawRectangle(RECTA rect) {
    glVertex2f(rect.right, rect.top);
    glVertex2f(rect.left, rect.top);
   glEnd();
+}
+
+void drawFilledCircle(GLfloat x, GLfloat y, GLfloat radius){
+	int i;
+	int triangleAmount = 20; //# of triangles used to draw circle
+	
+	//GLfloat radius = 0.8f; //radius
+	GLfloat twicePi = 2.0f * PI;
+	
+	glBegin(GL_TRIANGLE_FAN);
+		glVertex2f(x, y); // center of circle
+		for(i = 0; i <= triangleAmount;i++) { 
+			glVertex2f(x + (radius * cos(i *  twicePi / triangleAmount)),y + (radius * sin(i * twicePi / triangleAmount)));
+		}
+	glEnd();
 }
 //Atualiza posição das barras dos jogadores
 void updatePosPlayer() {
@@ -220,7 +236,7 @@ void Render() {
 
   DrawRectangle(player_1);
   DrawRectangle(player_2);
-
+  drawFilledCircle(200,500,20)
   if (checkGollPlayer1(ball, player_1))
     Xspeed = delta;
   if (checkgollPlayer2(ball, player_2))
